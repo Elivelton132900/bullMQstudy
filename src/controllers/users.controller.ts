783364560cbc0b1578443b3f1db795cd78813556
Elivelton132900 <elivelton1329@gmail.com>
2026-01-18 13:31:59 -0300

    removeOnComplete and RemoveOnFail

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -5,7 +5,7 @@ import IORedis from "ioredis"
 export class Users {
 
     static async store(req: Request, res: Response) {
-        const { full_name, password, email } = req.body
+        const payload = req.body
 
         // pode-se passar {host, port, username, password}
         const connection = new IORedis({
@@ -13,17 +13,26 @@ export class Users {
             port: 6379
         }) as ConnectionOptions
 
-        const queue = new Queue("createUser", {connection})
+        // segundo parâmetro configurações de conexão. host, porta, username e password
+        const queue = new Queue("userStoreQueue", { connection })
 
         // o primeiro parâmetro é o nome do job
-        const add: Job = await queue.add("registerUserJob", { full_name, password, email })
+        const add: Job = await queue.add("UserStoreJob", payload, {
+            // removeOnComplete e RemoveOnFail: {}
+            // se true, deleta assim que o job for executado com sucesso. se age, passa tempo em segundos para que esse job seja apagado. se count, guarda os últimos x
+            // ao utilizar count e age, guarda os ultimos x job por y quantidade de segundos.
+            removeOnComplete:{
+                age: 3600
+            }, 
+            removeOnFail: {
+                count: 1000
+            }
+
+        })
         console.log(add)
         res.status(200).json({
             mesasge: "User created",
-            full_name,
-            email,
-            password
-        })
 
+        })
     }
 }
