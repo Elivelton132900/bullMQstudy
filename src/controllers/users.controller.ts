4065a84ec751b91e1799643eaf8d7d622fabaff2
Elivelton132900 <elivelton1329@gmail.com>
2026-01-18 14:04:19 -0300

    addBulk, global concurrency and job cleaning

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,6 +1,7 @@
 import { ConnectionOptions, Job, Queue } from "bullmq";
 import { Request, Response } from "express";
 import IORedis from "ioredis"
+import { nanoid } from "nanoid";
 
 export class Users {
 
@@ -18,17 +19,42 @@ export class Users {
 
         // o primeiro parâmetro é o nome do job
         const add: Job = await queue.add("UserStoreJob", payload, {
+            jobId: nanoid() as string, //idempotência. Se um job com esse mesmo valor for enviado à fila, ele não será adicionado.
             // removeOnComplete e RemoveOnFail: {}
             // se true, deleta assim que o job for executado com sucesso. se age, passa tempo em segundos para que esse job seja apagado. se count, guarda os últimos x
             // ao utilizar count e age, guarda os ultimos x job por y quantidade de segundos.
             removeOnComplete:{
-                age: 3600
+                count: 300,
+                age: 3600 * 24
             }, 
             removeOnFail: {
-                count: 1000
+                count: 1000,
+                age: 3600 * 24
             }
 
         })
+
+        // ---------------------------------------------- adição em massa de jobs, concorrência global e deletando jobs da fila --------------------------------------------------
+
+        // adicionando vários jobs de uma vez
+        const newQueue: Queue = new Queue("NewUserStoreQueue")
+
+        // Permite que dentro dessa nova fila, apenas seja permitido que um worker trabalhe simultaneamente em 4 jobs
+        await newQueue.setGlobalConcurrency(4)
+
+        // limpar os jobs que estão na fila com o status de waiting ou delayed
+        await newQueue.drain()
+
+        // limpa TODA a fila
+        await newQueue.obliterate()
+        await newQueue.addBulk([
+            {name: "Eli", data: {send: "contact@geral.com.br", subject: "about you account"}},
+            {name: "Eli", data: {send: "contact@geral.com.br", subject: "about you account"}},
+            {name: "Eli", data: {send: "contact@geral.com.br", subject: "about you account"}},
+            {name: "Eli", data: {send: "contact@geral.com.br", subject: "about you account"}},
+            {name: "Eli", data: {send: "contact@geral.com.br", subject: "about you account"}}
+        ])
+
         console.log(add)
         res.status(200).json({
             mesasge: "User created",
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,8 @@
 import express from "express"
 import { userRegister } from "./users.route"
 
-console.log("ESTOU AQUI 2")
-
 export const routes = (app: express.Express) => 
     {
-    console.log("REGISTERING ROUTES")
     app.use(express.json({ limit: "5mb" }))
     app.use("/api", userRegister)
 }
